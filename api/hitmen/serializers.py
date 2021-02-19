2ffcad507eb0cc32a5d981827d5e1a712b4b5278
Isaac Cruz Ramirez <icruzr93@gmail.com>
2021-02-19 14:55:12 -0600

    back: apps created

--- api/hitmen/serializers.py
+++ api/hitmen/serializers.py
@@ -1,25 +1,7 @@
 from rest_framework import serializers
 from hitmen.models import Hitmen
 
-
-class HitmenSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    first_name = serializers.CharField(allow_blank=False, max_length=100)
-    last_name = serializers.CharField(allow_blank=False, max_length=100)
-    email = serializers.EmailField(allow_blank=False, max_length=100)
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Hitmen` instance, given the validated data.
-        """
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Hitmen` instance, given the validated data.
-        """
-        instance.first_name = validated_data.get('first_name', instance.first_name)
-        instance.last_name = validated_data.get('last_name', instance.last_name)
-        instance.email = validated_data.get('email', instance.email)
-        instance.save()
-        return instance
+class HitmenSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'frist_name', 'last_name', 'email']
--- api/hits/serializers.py
+++ api/hits/serializers.py
@@ -1,25 +1,7 @@
 from rest_framework import serializers
 from hits.models import Hit, HIT_STATES
 
-
-class HitSerializer(serializers.Serializer):
-    id = serializers.IntegerField(read_only=True)
-    target_name = serializers.CharField(required=True, max_length=100)
-    assigned_to = serializers.CharField(required=True, max_length=100)
-    state = serializers.ChoiceField(choices=HIT_STATES, default='in_progress')
-
-    def create(self, validated_data):
-        """
-        Create and return a new `Hit` instance, given the validated data.
-        """
-        return Snippet.objects.create(**validated_data)
-
-    def update(self, instance, validated_data):
-        """
-        Update and return an existing `Hit` instance, given the validated data.
-        """
-        instance.target_name = validated_data.get('target_name', instance.target_name)
-        instance.assigned_to = validated_data.get('assigned_to', instance.assigned_to)
-        instance.state = validated_data.get('state', instance.state)
-        instance.save()
-        return instance
+class HitSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ['id', 'target_name', 'assigned_to', 'state']
